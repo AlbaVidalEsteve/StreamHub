02e36d6c01e2ca00b074cfc52a8cbda079ff2e7c
Alba <vidalba97@gmail.com>
2024-01-26 11:36:36 +0100

    Update de clase
    creem signup spanish i planform spanish
    millorem css de totes les pagines
    modifiquem select pero encara no acabat

--- js/script.js
+++ js/script.js
@@ -14,23 +14,7 @@ let seleccion = document.querySelector('#valores');
 seleccion.addEventListener('change', function(){
     let opcionSeleccionada = this.options[this.selectedIndex];
 
-    if(opcionSeleccionada.value == 'Espanol'){
-        console.log('Opcion seleccionada Español');
-        window.location.assign('violetafilms.html');
-        
-    }else{
-        console.log('Opcion seleccionada Ingles');
-        window.location.assign('violetafilms-english.html');
-    }
-
-});
-
-let otroseleccion = document.getElementById('lang');
-console.log(otroseleccion)
-otroseleccion.addEventListener('change', function(){
-    let opcionSeleccionada = this.options[this.selectedIndex];
-
-    if(opcionSeleccionada.value == 'Espanol'){
+    if(opcionSeleccionada.value == 'Español'){
         console.log('Opcion seleccionada Español');
         window.location.assign('violetafilms.html');
         
@@ -43,29 +27,22 @@ otroseleccion.addEventListener('change', function(){
 
 
 
-// // Seleccionar los botones
-// let botones = document.querySelectorAll('.tablinks');
-// // Esperar a que se produzca click
-// botones.forEach(boton => {
-//     // boton.addEventListener('click', (event) => saludo(event,boton));
-//     boton.addEventListener('click', (evento) => openTab(evento,boton));
-    
-// });
 
-// //Funcion abrir el contenido de las pestañas
-// const openTab = (evt,element) =>{   
-//     console.log(element.getAttribute('id')); 
-//     //Declaración de variables
-//     let tabContents = document.querySelectorAll('.tab-content');
-//     let tabLinks = document.querySelectorAll('tablinks');
-//     let tabId = element.getAttribute('id');
+document.addEventListener('DOMContentLoaded', function () {
+    let otroseleccion = document.getElementById('lang');
+    // console.log(otroseleccion);
 
-//     //Inicializar todos los nodos con un display:none
-//     tabContents.forEach(content => content.style.display = 'none');
-//     //Inicializar todos los botones quitando la class .active
-//     tabLinks.forEach(link => link.className = link.className.replace('active',''));
-//     //Visualizar el contenido de la pestaña (seleccionada selecciona contendio por ID)
-//     console.log(document.getElementById(`${tabId}Content`))
-//     document.getElementById(`${tabId}Content`).style.display='block';
-//     evt.currentTarget.className += 'active'; //
-// }
+    otroseleccion.addEventListener('change', function(){
+        let opcionSeleccionada = this.options[this.selectedIndex];
+
+        if(opcionSeleccionada.value == 'Español'){
+            console.log('Opcion seleccionada Español');
+            window.location.assign('violetafilms.html');
+            
+        } else {
+            console.log('Opcion seleccionada Ingles');
+            window.location.assign('violetafilms-english.html');
+        }
+    });
+});
+
