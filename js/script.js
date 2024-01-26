3a7e78540a3a40d9f52d0c38da4a13723fd6e919
Alba <vidalba97@gmail.com>
2024-01-26 11:58:39 +0100

    New JS for language

--- js/script.js
+++ js/script.js
@@ -9,40 +9,81 @@ contenedores.forEach(contenedor => {
 
 
 //Selected language
-let seleccion = document.querySelector('#valores');
+// let seleccion = document.querySelector('#valores');
 
-seleccion.addEventListener('change', function(){
-    let opcionSeleccionada = this.options[this.selectedIndex];
+// seleccion.addEventListener('change', function(){
+//     let opcionSeleccionada = this.options[this.selectedIndex];
 
-    if(opcionSeleccionada.value == 'Español'){
-        console.log('Opcion seleccionada Español');
-        window.location.assign('violetafilms.html');
+//     if(opcionSeleccionada.value == 'Español'){
+//         console.log('Opcion seleccionada Español');
+//         window.location.assign('violetafilms.html');
         
-    }else{
-        console.log('Opcion seleccionada Ingles');
-        window.location.assign('violetafilms-english.html');
-    }
+//     }else{
+//         console.log('Opcion seleccionada Ingles');
+//         window.location.assign('violetafilms-english.html');
+//     }
 
-});
-
-
+// });
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    let otroseleccion = document.getElementById('lang');
-    // console.log(otroseleccion);
 
-    otroseleccion.addEventListener('change', function(){
-        let opcionSeleccionada = this.options[this.selectedIndex];
 
-        if(opcionSeleccionada.value == 'Español'){
-            console.log('Opcion seleccionada Español');
-            window.location.assign('violetafilms.html');
+// document.addEventListener('DOMContentLoaded', function () {
+//     let otroseleccion = document.getElementById('lang');
+//     console.log(otroseleccion);
+
+//     otroseleccion.addEventListener('change', function(){
+//         let opcionSeleccionada = this.options[this.selectedIndex];
+
+//         if(opcionSeleccionada.value == 'Español'){
+//             console.log('Opcion seleccionada Español');
+//             window.location.assign('violetafilms.html');
             
-        } else {
-            console.log('Opcion seleccionada Ingles');
-            window.location.assign('violetafilms-english.html');
+//         } else {
+//             console.log('Opcion seleccionada Ingles');
+//             window.location.assign('violetafilms-english.html');
+//         }
+//     });
+// });
+
+/**fins aqui v1 */
+
+
+let seleccion = document.querySelectorAll('.valores');
+
+seleccion.forEach((selector) => {
+    selector.addEventListener('change', (e) => {
+        let idioma = e.target.value;
+        console.log(idioma);
+
+        switch (idioma) {
+            case '1':
+                window.location.assign('violetafilms.html');
+                break;
+            case '2':
+                window.location.assign('violetafilms-english.html');
+                break;
+            case '3':
+                window.location.assign('signupSP.html');
+                break;
+            case '4':
+                window.location.assign('singup.html');
+                break;
+            case '5':
+                window.location.assign('planform2.html');
+                break;
+            case '6':
+                window.location.assign('planform.html');
+                break;
+            case '7':
+                window.location.assign('login.html');
+                break;
+            case '8':
+                window.location.assign('login2.html');
+                break;
+            default:
+                console.log(idioma);
+                window.location.assign('#');
         }
     });
 });
-
